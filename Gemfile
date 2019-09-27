d2182d9809e9210ff16f4a84b85327a83232dc2e
louisbizouerne <louis.bizouerne@coupa.com>
2019-09-27 14:13:44 +0100

    more changed

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in startandkonnect.gemspec
-gem "aws-sdk"
+gem "aws-sdk-ec2"
 gemspec
--- lib/startandkonnect.rb
+++ lib/startandkonnect.rb
@@ -6,18 +6,22 @@ module Startandkonnect
   
   class AWS
 
-    def initialize(region)
+    def initialize(in_region)
       config_file = File.join(File.dirname(File.expand_path(__FILE__)), %w( .. config aws_config.yml))
       if File.exists?(config_file)
         @@aws_config = YAML.load_file(config_file)
         access_key = @@aws_config["AWS_ACCESS_KEY_ID"]
         secret_key = @@aws_config['AWS_SECRET_ACCESS_KEY']
+        region = @@aws_config['AWS_REGION']
       elsif (ENV["AWS_ACCESS_KEY_ID"] && ENV["AWS_SECRET_ACCESS_KEY"])
         access_key = ENV["AWS_ACCESS_KEY_ID"]
         secret_key = ENV["AWS_SECRET_ACCESS_KEY"]
+        region = ENV['AWS_REGION']
       else
         raise "AWS credentials not found!"
       end
+      region = in_region unless in_region.nil?
+
       credentials = Aws.config[:credentials] = Aws::Credentials.new(access_key, secret_key)
       @client = Aws::EC2::Client.new(region: region, credentials: credentials)
     end
@@ -26,5 +30,33 @@ module Startandkonnect
       resp = @client.describe_instances({filters: [{name: "tag:Name", values: query}]})
     end
 
+    def start_instance(instance_id)
+      i = ec2.instance(instance_id)
+      if i.exists?
+        case i.state.code
+        when 0  # pending
+          r = "#{id} is pending, so it will be running in a bit"
+        when 16  # started
+          r = "#{id} is already started"
+        when 48  # terminated
+          r = "#{id} is terminated, so you cannot start it"
+        else
+          i.start
+          puts i.inspect
+          r = "#{id} is starting"
+        end
+      else
+        r = "#{instance_id} does not exist!"
+      end
+      return r
+    end
+
+  end
+
+  class SSH 
+
+    def connect(server)
+      `ssh -i ~/Documents/kali_perso.pem ec2-user@#{server}`
+    end
   end
 end
